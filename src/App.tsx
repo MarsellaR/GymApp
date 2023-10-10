9eb76b902841e82227de96b27bc3236495bd1f84
Marsella/R <rozic.marsela@gmail.com>
2023-10-10 12:57:41 +0200

    new

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@
 
 
 
-
+import Navbar from "@/scenes/navbar";/* automatski prepoznaje da se radi o fajlu index.tsx ne trebamo dodatno pisati /index.tsx */
 function App() {
 
 
   return (
     <>
-      <div className='app'>
-   +app
+      <div className='app bg-gray20' >
+<Navbar/>
       </div>
   
     </>
