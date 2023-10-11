9d954c35f8ffc189af59e3452214f4d7afee24ae
Marsella/R <rozic.marsela@gmail.com>
2023-10-11 12:54:02 +0200

    new

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
+import { useState } from "react";
+import Navbar from "@/scenes/navbar"; /* automatski prepoznaje da se radi o fajlu index.tsx ne trebamo dodatno pisati /index.tsx */
+import { SelectedPage } from "@/shared/types";
 
 
 
-
-import Navbar from "@/scenes/navbar";/* automatski prepoznaje da se radi o fajlu index.tsx ne trebamo dodatno pisati /index.tsx */
 function App() {
-
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
 
   return (
     <>
-      <div className='app bg-gray20' >
-<Navbar/>
+      <div className="app bg-gray20">
+        <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
       </div>
-  
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -3,39 +3,56 @@
 
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import Logo from "@/assets/Logo.png";
-type Props = {}
+import { LinkHTMLAttributes } from "react";
+import Link from "./Link";
+import { SelectedPage } from "@/shared/types";
 
-const Navbar = (props: Props) => {
+type Props = { selectedPage: SelectedPage; setSelectedPage: (value:SelectedPage) => void };
+
+const Navbar = ({selectedPage,setSelectedPage}: Props) => {
   const flexBetween = "flex items-center justify-between";
   return (
-<nav> 
-    
-    <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`} >
-    <div  className={`${flexBetween} mx-auto w-5/6`} >
- <div className={`${flexBetween} w-full gap-16`}  >
-   {/* left side */}
-<img alt="logo" src={Logo} />
-{/* right side */}
-<div className={`${flexBetween} w-full`}>
-<div className={`${flexBetween} gap-8 text-sm`} >
-    <p>Home</p>
-        <p>Benefits</p>
-            <p>Our Classes</p>
-                <p>Contact Us</p>
+    <nav>
+      <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
+        <div className={`${flexBetween} mx-auto w-5/6`}>
+          <div className={`${flexBetween} w-full gap-16`}>
+            {/* left side */}
+            <img alt="logo" src={Logo} />
+            {/* right side */}
+            <div className={`${flexBetween} w-full`}>
+              <div className={`${flexBetween} gap-8 text-sm`}>
+                <Link
+                  page="Home"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+                <Link
+                  page="Benefits"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+                <Link
+                  page="Our Classes"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+                <Link
+                  page="Contact Us"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+              </div>
 
-  </div>
-
-   <div >
-<p>Sign In</p>
-<button>Become a Member</button>
-  
-   </div>
-    </div>
-    </div>
-</div>
-</div>
+              <div>
+                <p>Sign In</p>
+                <button>Become a Member</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </nav>
-  )
+  );
 }
 
 export default Navbar;
