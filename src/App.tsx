e01ba009006e8440ab32ca0cc5678a9a0a2dd45b
Marsella/R <rozic.marsela@gmail.com>
2023-10-14 13:33:47 +0200

    new

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Navbar from "@/scenes/navbar"; /* automatski prepoznaje da se radi o fajlu index.tsx ne trebamo dodatno pisati /index.tsx */
 import { SelectedPage } from "@/shared/types";
 
-
-
 function App() {
-  const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
-
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
+  const [isTopPOfPage, setisTopPOfPage] = useState<boolean>(true);
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setisTopPOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      }
+      if (window.scrollY !== 0) setisTopPOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  },[]);
   return (
     <>
       <div className="app bg-gray20">
-        <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+        <Navbar isTopOfPage={isTopPOfPage} selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
       </div>
     </>
   );
